27f8076d3bb352a79a8c91549e0e63940cc9247c
Jason C Armstrong <bearfootcomputing@gmail.com>
2018-11-13 16:38:12 -0700

    Added assignment pdf, starting java files with intro comments

---
+++ Mazes/SmartSolve.java
@@ -0,0 +1,35 @@
+
+/**
+ * Assignment 2: Constraint Satisfaction Problem: FlowFree - smart version
+ * This is the "smart" version which includes any variable and value ordering heuristics that make
+ * sense for your formulation (which would probably include forward checking).
+ * We will need to figure out how we want to do this, of course.
+ * Jason Armstrong, Beau Anderson, Robert Tiller, Kyle Ungersma
+ * Nov 13, 2018
+ */
+public class SmartSolve
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class SmartSolve
+     */
+    public SmartSolve()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
